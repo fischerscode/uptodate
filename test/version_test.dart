0e145843da0c01ef0a0332725742010a0af36216
Maximilian Fischer <github@maaeps.de>
2021-09-28 01:39:57 +0200

    Create version_test.dart

---
+++ test/version_test.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+import 'package:uptodate/src/command/version.dart';
+import 'package:yamltools/yamltools.dart';
+import 'package:test/test.dart';
+import 'package:yaml/yaml.dart';
+
+main() {
+  test('Test version', () async {
+    var pubspec = await File('pubspec.yaml').readAsString();
+    var version = loadYamlNode(pubspec).getMapValue('version')?.asString();
+    // expect(version, TypeMatcher<String>());
+    // expect(VersionCommand.version, TypeMatcher<String>());
+    expect(VersionCommand.version.toString(), version);
+  });
+}

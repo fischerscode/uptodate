37cb7ed7dbf1805abd16f8c37fddc2129fc52f37
Maximilian Fischer <github@maaeps.de>
2021-09-28 16:41:10 +0200

    create SubstitutableString

---
+++ lib/src/tools/substitute.dart
@@ -0,0 +1,18 @@
+extension SubstitutableString on String {
+  /// Replace every occurrence of the [variable] marked by the [character]
+  /// with the [value], except the [character] is escaped by the [escapeCharacter].
+  String substitute(
+    String variable,
+    String value, {
+    String character = '\$',
+    String escapeCharacter = '\\',
+  }) {
+    return replaceAll('$escapeCharacter$escapeCharacter',
+            '$escapeCharacter$escapeCharacter ')
+        .replaceAll('$escapeCharacter$character',
+            '$escapeCharacter$character$escapeCharacter')
+        .replaceAll('$character$variable', value)
+        .replaceAll('$escapeCharacter$character$escapeCharacter', '$character')
+        .replaceAll('$escapeCharacter$escapeCharacter ', '$escapeCharacter');
+  }
+}
---
+++ test/substitude_test.dart
@@ -0,0 +1,42 @@
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+import 'package:uptodate/src/tools/substitute.dart';
+
+main() {
+  test('test substitution', () {
+    expect('hello \$name!'.substitute('name', 'octocat'), 'hello octocat!');
+    expect('hello \\\$name!'.substitute('name', 'octocat'), 'hello \$name!');
+    expect(
+        'hello \\\$\\name!'.substitute('name', 'octocat'), 'hello \$\\name!');
+    expect(
+        'hello \\\\\$name!'.substitute('name', 'octocat'), 'hello \\octocat!');
+  });
+  test('test substitution with custom character', () {
+    expect('hell\\o oname!'.substitute('name', 'octocat', character: 'o'),
+        'hello octocat!');
+    expect('hell\\o \\oname!'.substitute('name', 'octocat', character: 'o'),
+        'hello oname!');
+    expect('hell\\o \\o\\name!'.substitute('name', 'octocat', character: 'o'),
+        'hello o\\name!');
+    expect('hello \\\\oname!'.substitute('name', 'octocat', character: 'o'),
+        'hello \\octocat!');
+  });
+  test('test substitution with custom character and escapeCharacter', () {
+    expect(
+        'helljo oname!'.substitute('name', 'octocat',
+            character: 'o', escapeCharacter: 'j'),
+        'hello octocat!');
+    expect(
+        'helljo joname!'.substitute('name', 'octocat',
+            character: 'o', escapeCharacter: 'j'),
+        'hello oname!');
+    expect(
+        'helljo jojname!'.substitute('name', 'octocat',
+            character: 'o', escapeCharacter: 'j'),
+        'hello ojname!');
+    expect(
+        'hello jjoname!'.substitute('name', 'octocat',
+            character: 'o', escapeCharacter: 'j'),
+        'hello joctocat!');
+  });
+}

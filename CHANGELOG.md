08a0a1090b1d4731fd51e4335f6ff278ca6622b5
Maximilian Fischer <github@maaeps.de>
2021-09-28 15:28:21 +0200

    update version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2
+
+- Rename action to up-to-date
+
 ## 0.0.1
 
 - Initial version
--- lib/src/command/version.dart
+++ lib/src/command/version.dart
@@ -9,7 +9,7 @@ class VersionCommand extends Command<int> {
   @override
   final description = 'Print uptodate version.';
 
-  static final Version version = Version(0, 0, 1);
+  static final Version version = Version(0, 0, 2);
 
   @override
   Future<int> run() async {

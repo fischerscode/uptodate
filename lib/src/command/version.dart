8eef15d9ce7c780689c805571afcaa5356ed9ec6
Maximilian Fischer <github@maaeps.de>
2021-09-28 16:42:38 +0200

    set version to 0.1.0

--- lib/src/command/version.dart
+++ lib/src/command/version.dart
@@ -9,7 +9,7 @@ class VersionCommand extends Command<int> {
   @override
   final description = 'Print uptodate version.';
 
-  static final Version version = Version(0, 0, 2);
+  static final Version version = Version(0, 1, 0);
 
   @override
   Future<int> run() async {

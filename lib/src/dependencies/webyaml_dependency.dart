d68b4abf1b9aedcf9339344cccbc1880924884cb
Maximilian Fischer <github@maaeps.de>
2021-11-21 13:28:03 +0100

    add yamlPathResolver

--- lib/src/dependencies/webyaml_dependency.dart
+++ lib/src/dependencies/webyaml_dependency.dart
@@ -1,13 +1,15 @@
 part of dependencies;
 
-class WebYamlDependency extends WebJsonDependency {
+class WebYamlDependency extends WebDependency {
   static const String identifier = 'webyaml';
+
+  final String path;
 
   WebYamlDependency({
     required final String name,
     required final String currentVersion,
     required final Uri url,
-    required String path,
+    required this.path,
     required String prefix,
     required String? issueTitle,
     required String? issueBody,
@@ -17,25 +19,40 @@ class WebYamlDependency extends WebJsonDependency {
           name: name,
           currentVersion: currentVersion,
           url: url,
-          path: path,
           prefix: prefix,
           issueTitle: issueTitle,
           issueBody: issueBody,
           versionExtractor: versionExtractor ??
-              (response) =>
-                  WebJsonDependency.jsonVersionExtractor(path, prefix)(
-                    http.Response(
-                      jsonEncode(loadYaml(response.body)),
-                      response.statusCode,
-                      headers: response.headers,
-                      isRedirect: response.isRedirect,
-                      persistentConnection: response.persistentConnection,
-                      reasonPhrase: response.reasonPhrase,
-                      request: response.request,
-                    ),
-                  ),
+              (response) {
+                var yaml = loadYaml(response.body);
+
+                var version = yamlPathResolver(yaml, path);
+                if (version.startsWith(prefix)) {
+                  version = version.substring(prefix.length);
+                }
+                return Version.parse(version);
+              },
           issueLabels: issueLabels,
         );
+
+  static String yamlPathResolver(dynamic yaml, String path) {
+    for (final key in path.split('.')) {
+      if (yaml is YamlMap && yaml.containsKey(key)) {
+        yaml = yaml[key];
+      } else if (yaml is YamlList &&
+          int.tryParse(key) != null &&
+          yaml.length > int.parse(key)) {
+        yaml = yaml[int.parse(key)];
+      } else {
+        throw 'No version found!';
+      }
+    }
+    if (yaml is String) {
+      return yaml;
+    } else {
+      throw 'No version found!';
+    }
+  }
 
   static GenericDependency? parse({
     required String name,
